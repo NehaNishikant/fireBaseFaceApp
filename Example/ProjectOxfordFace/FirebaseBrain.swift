b75f68417aceffacaf939edd1c90323a8292bd3d
NehaNishikant <31891939+NehaNishikant@users.noreply.github.com>
2018-06-05 10:36:50 -0400

    first

---
+++ Example/ProjectOxfordFace/FirebaseBrain.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBrain.swift
+//  ProjectOxfordFace_Example
+//
+//  Created by Neha Nish on 6/1/18.
+//  Copyright © 2018 David Porter. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseBrain: NSObject {
+    
+    func save(){
+        print("works")
+    }
+
+}
